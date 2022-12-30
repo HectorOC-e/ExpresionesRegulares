07ffe1936fc4c377189d151e011154d62229469a
HectorOC-e <osoriohector89@gmail.com>
2022-12-29 22:40:24 -0600

    Agregar archivos de proyecto.

---
+++ ExpresionesRegulares/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExpresionesRegulares
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string frase = "Mi web es https://www.pildorasinformaticas.es";
+            Console.WriteLine();
+
+            string pattern = "(https?://)?(www.)?pildorasinformaticas.es";
+
+            Regex miRegex = new Regex(pattern);
+
+            MatchCollection matchCollection= miRegex.Matches(frase);
+
+            if (matchCollection.Count > 0) Console.WriteLine("Se ha encontrado una web");
+            else Console.WriteLine("No se ha encontrado una web");
+
+            
+            
+        }
+    }
+}
